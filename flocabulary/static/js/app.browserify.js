adabd2a2c8b05cba0950dd568ffc0cb05175b235
Chris Henry <henry.christopher@gmail.com>
2016-05-08 15:27:46 -0400

    new: Wire up form

--- flocabulary/static/js/app.browserify.js
+++ flocabulary/static/js/app.browserify.js
@@ -1,13 +1,58 @@
 'use strict';
 
-var TestApp = React.createClass({
+var ShortenForm = React.createClass({
+  initialInputValue: "Enter a URL. e.g. https://www.google.com",
+
+  getInitialState: function() {
+    return {
+      long_url: this.initialInputValue
+    };
+  },
+
+  handleChange: function(evt) {
+    this.setState({
+      long_url: evt.target.value
+    });
+  },
+
+  reset: function() {
+    this.setState({
+      long_url: this.initialInputValue
+    });
+  },
+
+  alertValue: function() {
+    alert(this.state);
+  },
+
+  onFocus: function () {
+    if(this.state.long_url == this.initialInputValue) {
+      this.setState({
+        long_url: ""
+      });
+    }
+  },
+
+  onBlur: function () {
+    if(this.state.long_url.trim() == "") {
+      this.setState({
+        long_url: this.initialInputValue
+      });
+    }
+  },
+
+  onSubmit: function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(this.state.long_url)
+  },
+
   render: function() {
-    console.log("herer")
     return (
-      <form id="long-url-form">
+      <form id="long-url-form" onSubmit={this.onSubmit}>
         <div className="input-group col-md-8 col-centered">
-          <label for="long_url" class="sr-only">Url to shorten:</label>
-          <input name="long_url" type="text" className="form-control" defaultValue="Enter a URL. e.g. https://www.google.com" />
+          <label for="long_url">Url to shorten:</label>
+          <input name="long_url" type="text" className="form-control" value={this.state.long_url} onChange={this.handleChange} onFocus={this.onFocus} onBlur={this.onBlur}/>
           <span className="input-group-btn">
             <button className="btn btn-primary">Shorten</button>
           </span>
@@ -18,6 +63,6 @@ var TestApp = React.createClass({
 });
 
 ReactDOM.render(
-  React.createElement(TestApp, null),
+  React.createElement(ShortenForm, null),
   document.getElementById('content')
 );
